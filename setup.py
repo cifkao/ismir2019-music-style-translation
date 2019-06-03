7e015c7b1adc3e2898edb4623e36fb5cba55591a
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-06-03 14:34:31 +0200

    Require python 3.6

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setuptools.setup(
     author='Ondřej Cífka',
     description='The code for the paper "Supervised symbolic music style translation using synthetic data"',
     url='https://github.com/cifkao/music-style-translation',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=[
-        'museflow @ git+ssh://git@github.com/cifkao/museflow@45d8e8fd7fc03f7a95faba7b0e8ed13a6f0c8db4',
+        'museflow @ git+ssh://git@github.com/cifkao/museflow@c14f94ac096743d5a7a83afccfa098853b9c97e9',
         'scipy'
     ],
     packages=setuptools.find_packages(),
